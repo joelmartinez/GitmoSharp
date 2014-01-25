62740fa6c4fbe3e8fc258cc2f1201d8078cb1ff0
Joel Martinez <joelmartinez@gmail.com>
2014-01-25 13:03:06 -0500

    Refactored unit test startup and cleanup

--- GitmoSharp.Test/GitmoTest.cs
+++ GitmoSharp.Test/GitmoTest.cs
@@ -8,20 +8,40 @@ namespace GitmoSharp.Test
     [TestClass]
     public class GitmoTest
     {
+        private string[] paths = { 
+                                     "Test/NotInitialized", //0
+                                      "Test/Initialized"    //1
+                                 };
         [TestInitialize]
         public void TestInit()
         {
-            IO.Directory.CreateDirectory("Test/NotInitialized");
+            foreach (var path in paths) {
+                IO.Directory.CreateDirectory(path);
+            }
+        }
 
-
+        [TestCleanup]
+        public void TestClean()
+        {
+            foreach (var path in paths) {
+                IO.Directory.Delete(path, true);
+            }
         }
 
         [TestMethod]
         public void TestIsValid()
         {
-            bool result = Gitmo.IsValid("Tests/NotInitialized");
+            bool result = Gitmo.IsValid(paths[0]);
 
             Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestIsValid_True()
+        {
+            string path = paths[1];
+            Gitmo.Init(path);
+            Assert.IsTrue(Gitmo.IsValid(path));
         }
     }
 }
